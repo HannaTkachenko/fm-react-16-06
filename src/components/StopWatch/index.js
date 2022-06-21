5c21e6651795545b4b42e0bcc640bd0638b7480c
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-06-21 11:35:13 +0300

    Example setState with callback

--- src/components/StopWatch/index.js
+++ src/components/StopWatch/index.js
@@ -3,10 +3,16 @@ import React, { Component } from 'react';
 class StopWatch extends Component {
   constructor(props) {
     super(props);
-    this.state = {count:0};
+    this.state = {time:new Date(0,0,0,0,0,0)};
     this.idInterval = null;
   }
-  tick=()=>{this.setState({count:this.state.count+1})}
+  tick=()=>{
+    this.setState((state)=>{
+      const {time} = state;
+      const newTime = new Date(time.getTime()+1000);
+      return {time:newTime};
+    })
+  }
   start = ()=>{
     this.stop();
     this.idInterval = setInterval(this.tick, 1000)
@@ -17,7 +23,7 @@ class StopWatch extends Component {
   }
   reset = ()=>{
     this.stop();
-    this.setState({count:0})
+    this.setState({time:new Date(0,0,0,0,0,0)})
   }
 
   componentDidMount(){
@@ -30,10 +36,10 @@ class StopWatch extends Component {
   }
   
   render() {
-    const {count} = this.state;  
+    const {time} = this.state;  
     return (
       <article>
-        <h2>{count}</h2>
+        <h2>{time.toLocaleTimeString('it-IT')}</h2>
         <button onClick={this.start}>START</button>
         <button onClick={this.reset}>RESET</button>
         <button onClick={this.stop}>STOP</button>
