cca97ac5faa3592dd2d1548786ceee6b72b17842
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-06-24 10:54:43 +0300

    Add defaultProps

--- src/components/UserSection/UserCard/index.js
+++ src/components/UserSection/UserCard/index.js
@@ -3,8 +3,7 @@ import { PropTypes } from 'prop-types';
 
 
 const UserCard = (props) => {
-  const { user:{id, fname, isSelected}, 
-  userSelector=()=>{} } = props;
+  const { user:{id, fname, isSelected}, userSelector } = props;
   const styles = { border: isSelected ? "solid teal 2px" : undefined };
   const btnHandler=()=>userSelector(id);
   return (
@@ -17,6 +16,11 @@ const UserCard = (props) => {
   );
 };
 
+UserCard.defaultProps = {
+  userSelector: ()=>{},
+  user:{id:0, fname:'Anonim', isSelected:false}
+}
+
 export const userShape = PropTypes.shape({
   id:PropTypes.number.isRequired,
   fname: PropTypes.string.isRequired,
@@ -27,5 +31,7 @@ UserCard.propTypes = {
   user: userShape.isRequired,
   userSelector: PropTypes.func,
 }
+
+
 
 export default UserCard;
