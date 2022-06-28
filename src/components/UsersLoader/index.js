983f1f3695a03cd61bd11770887ee8b875f9c88d
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-06-28 11:54:09 +0300

    Add pagination

--- src/components/UsersLoader/index.js
+++ src/components/UsersLoader/index.js
@@ -5,8 +5,9 @@ class UsersLoader extends Component {
     super(props);
     this.state = {
       users: [],
-      isFetching: false,
+      isFetching: true,
       isError: false,
+      pageNum: 1
     };
   }
 
@@ -17,8 +18,8 @@ class UsersLoader extends Component {
   );
 
   componentDidMount() {
-    this.setState({ isFetching: true });
-    fetch("https://randomuser.me/api/")
+    const {pageNum} = this.state;
+    fetch(`https://randomuser.me/api/?results=15&page=${pageNum}&seed=users&nat=gb`)
       .then((response) => response.json())
       .then((data) =>
         this.setState({
@@ -36,9 +37,37 @@ class UsersLoader extends Component {
         })
       })
   }
+  componentDidUpdate(prevProps, prevState) {
+    const {pageNum} = this.state;
+    if (pageNum !== prevState.pageNum){  
+    fetch(`https://randomuser.me/api/?results=15&page=${pageNum}&seed=users&nat=gb`)
+      .then((response) => response.json())
+      .then((data) =>
+        this.setState({
+          users: data.results,
+        })
+      )
+      .catch((err) =>
+        this.setState({
+          isError: true,
+        })
+      )
+      .finally(()=>{
+        this.setState({
+          isFetching: false,
+        })
+      })
+    }
+  }
+  prevPage=()=>{
+    if(this.state.pageNum >1){
+      this.setState((state)=>({pageNum:state.pageNum-1}))
+    }
+  }
+  nextPage=()=>this.setState((state)=>({pageNum:state.pageNum+1}))
 
   render() {
-    const { users, isError, isFetching } = this.state;
+    const { users, isError, isFetching, pageNum } = this.state;
     if (isFetching) {
       return <p>Loading...</p>;
     }
@@ -48,6 +77,9 @@ class UsersLoader extends Component {
     return (
       <section>
         <h2>Users List</h2>
+        <button onClick={this.prevPage}>&lt;</button>
+        <button onClick={this.nextPage}>&gt;</button>
+        <span> page: {pageNum}</span>
         <ul>{users.map(this.showUser)}</ul>
       </section>
     );
