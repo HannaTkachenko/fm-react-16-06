87940b981cc2dd33ae25e6cfca77039eef9dd985
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-06-29 11:09:31 +0300

    Add select

--- src/components/UsersLoader/index.js
+++ src/components/UsersLoader/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import {getUsers}  from '../../api';
+import { getUsers } from "../../api";
 import Spinner from "../Spinner";
 
 class UsersLoader extends Component {
@@ -9,57 +9,58 @@ class UsersLoader extends Component {
       users: [],
       isFetching: true,
       isError: false,
-      pageNum: 1
+      pageNum: 1,
     };
   }
 
   showUser = (user) => (
-    <li key={user.login.uuid}> {JSON.stringify(user, null, 4)}
-      {/* {user.name.first} {user.name.last} (gender: {user.gender})
-      (nat: {user.nat}) */}
+    <li key={user.login.uuid}>
+      {/* {JSON.stringify(user, null, 4)} */}
+      {user.name.first} {user.name.last} (gender: {user.gender}) (nat:{" "}
+      {user.nat})
     </li>
   );
 
-  load=()=>{
-    const {pageNum} = this.state;
-    getUsers({page:pageNum, results:2, nat:'de', gender:'female'})
-      .then((data) =>{
-        if(data.error){
+  load = () => {
+    const { pageNum } = this.state;
+    getUsers({ page: pageNum, results: 2, nat: "de", gender: "female" })
+      .then((data) => {
+        if (data.error) {
           return this.setState({
             isError: true,
-          })
+          });
         }
         return this.setState({
           users: data.results,
-        })
+        });
       })
       .catch((err) =>
         this.setState({
           isError: true,
         })
       )
-      .finally(()=>{
+      .finally(() => {
         this.setState({
           isFetching: false,
-        })
-      })
-  }
+        });
+      });
+  };
 
   componentDidMount() {
     this.load();
   }
   componentDidUpdate(prevProps, prevState) {
-    const {pageNum} = this.state;
-    if (pageNum !== prevState.pageNum){  
-    this.load();
+    const { pageNum } = this.state;
+    if (pageNum !== prevState.pageNum) {
+      this.load();
     }
   }
-  prevPage=()=>{
-    if(this.state.pageNum >1){
-      this.setState((state)=>({pageNum:state.pageNum-1}))
+  prevPage = () => {
+    if (this.state.pageNum > 1) {
+      this.setState((state) => ({ pageNum: state.pageNum - 1 }));
     }
-  }
-  nextPage=()=>this.setState((state)=>({pageNum:state.pageNum+1}))
+  };
+  nextPage = () => this.setState((state) => ({ pageNum: state.pageNum + 1 }));
 
   render() {
     const { users, isError, isFetching, pageNum } = this.state;
@@ -76,7 +77,13 @@ class UsersLoader extends Component {
         <h2>Users List</h2>
         <button onClick={this.prevPage}>&lt;</button>
         <button onClick={this.nextPage}>&gt;</button>
-        <span> page: {pageNum}</span>
+        <span> page: {pageNum} </span>
+        <select>
+          <option value="ch">CH</option>
+          <option value="de">DE</option>
+          <option selected value="fr">FR</option>
+          <option value="gb">GB</option>
+        </select>
         <ul>{users.map(this.showUser)}</ul>
       </section>
     );
