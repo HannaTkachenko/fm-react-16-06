d951fa0cedcec6dc2009ec8158cdeb155e30222c
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-06-30 12:03:27 +0300

    Add react router dom

--- src/App.js
+++ src/App.js
@@ -1,15 +1,63 @@
 import React from "react";
-
+import { BrowserRouter, Link, Route, Routes, useNavigate } from "react-router-dom";
+import WindowSizes from "./components/WindowSizes";
+import Dashboard from './page/Dashboard';
+import Chao from './components/ChaoSection/Chao/index';
+import StopWatch from './components/StopWatch/index';
 
 const App = () => {
   return (
     <>
-      <button onClick={()=>{window.open('https://translate.google.com.ua/')}}>open</button>
-      <button onClick={()=>{window.close()}}>close</button>
-      <button onClick={()=>{window.location.assign('https://www.w3schools.com/')}}>assign</button>
-      <button onClick={()=>{window.location.replace('https://www.w3schools.com/')}}>replace</button>
-      <button onClick={()=>{window.location.reload()}}>reload</button>
+      <div>static start for application</div>
+      <BrowserRouter>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+            <li>
+              <Link to="/winsize">WindowSizes</Link>
+            </li>
+            <li>
+              <Link to="/dash/chao">chao</Link>
+            </li>
+            <li>
+              <Link to="/dash/stop">stop</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/winsize" element={<WindowSizes />} />         
+          <Route path="/dash/" element={<Dashboard />}>
+            <Route path="chao" element={<Chao name='elon' id={1}/>} />
+            <Route path="stop" element={<StopWatch />} />
+          </Route>
+          <Route path="/*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+      <div>static end for application</div>
     </>
   );
 };
+
+const Home = () => <section>HOME</section>;
+const About = () => <section>About</section>;
+const Contact = () => <section>Contact</section>;
+const PageNotFound = () => {
+  const navigate = useNavigate();
+  setTimeout(()=>{
+    navigate('/', { replace: true });
+  }, 2000);
+  return <section>404, replace to home!</section>;
+};
+
 export default App;
