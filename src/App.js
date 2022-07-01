d40c302d7aa7155baddaedeb5831708fe5858331
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-07-01 09:26:45 +0300

    Refactor App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from "react";
-import { BrowserRouter, Link, Route, Routes, useNavigate } from "react-router-dom";
+import {
+  BrowserRouter,
+  Link,
+  Route,
+  Routes
+} from "react-router-dom";
 import WindowSizes from "./components/WindowSizes";
-import Dashboard from './page/Dashboard';
-import Chao from './components/ChaoSection/Chao/index';
-import StopWatch from './components/StopWatch/index';
+import Dashboard from "./page/Dashboard";
+import Chao from "./components/ChaoSection/Chao/index";
+import StopWatch from "./components/StopWatch/index";
+import PageNotFound from './page/PageNotFound';
+
 
 const App = () => {
   return (
@@ -16,33 +23,31 @@ const App = () => {
               <Link to="/">Home</Link>
             </li>
             <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact</Link>
-            </li>
-            <li>
               <Link to="/winsize">WindowSizes</Link>
             </li>
             <li>
-              <Link to="/dash/chao">chao</Link>
-            </li>
-            <li>
-              <Link to="/dash/stop">stop</Link>
+              Dashboard
+              <ul>
+                <li>
+                  <Link to="/dash/chao">chao</Link>
+                </li>
+                <li>
+                  <Link to="/dash/stop">stop</Link>
+                </li>
+              </ul>
             </li>
           </ul>
         </nav>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/winsize" element={<WindowSizes />} />         
+          <Route path="/winsize" element={<WindowSizes />} />
           <Route path="/dash/" element={<Dashboard />}>
-            <Route path="chao" element={<Chao name='elon' id={1}/>} />
+            <Route path="chao" element={<Chao name="elon" id={1} />} />
             <Route path="stop" element={<StopWatch />} />
           </Route>
           <Route path="/*" element={<PageNotFound />} />
         </Routes>
+        <footer>@2022</footer>
       </BrowserRouter>
       <div>static end for application</div>
     </>
@@ -50,14 +55,6 @@ const App = () => {
 };
 
 const Home = () => <section>HOME</section>;
-const About = () => <section>About</section>;
-const Contact = () => <section>Contact</section>;
-const PageNotFound = () => {
-  const navigate = useNavigate();
-  setTimeout(()=>{
-    navigate('/', { replace: true });
-  }, 2000);
-  return <section>404, replace to home!</section>;
-};
+
 
 export default App;
