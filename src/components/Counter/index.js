3bd11255e2d687c5d59b90c74bc1f62579180833
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-07-01 10:57:00 +0300

    Example PureComponent

--- src/components/Counter/index.js
+++ src/components/Counter/index.js
@@ -1,37 +1,51 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import styles from './Counter.module.sass'
+import React, { PureComponent, Component } from "react";
+import PropTypes from "prop-types";
+import styles from "./Counter.module.sass";
 
-class Counter extends Component {
+class Counter extends PureComponent {
   constructor(props) {
     super(props);
     this.state = {
       count: 0,
-    }
+    };
   }
-  increment = ()=>{this.setState((state, props) => ({
-    count: state.count + props.step
-  }))}
-  decrement = ()=>{this.setState((state, props) => ({
-    count: state.count - props.step
-  }))}
-  
+  increment = () => {
+    this.setState((state, props) => ({
+      count: state.count + props.step,
+    }));
+  };
+  decrement = () => {
+    this.setState((state, props) => ({
+      count: state.count - props.step,
+    }));
+  };
+
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   return nextProps.step === this.props.step;
+  // }
+
   render() {
-    const {count} = this.state;
+    const { count } = this.state;
+    const { step } = this.props;
+    console.log("counter");
     return (
       <section className={styles.container}>
         <h2>count: {count}</h2>
+        <h2>step: {step}</h2>
         <button onClick={this.increment}>+</button>
         <button onClick={this.decrement}>-</button>
+        <button onClick={()=>{this.setState({count})}}>SET STATE</button>
       </section>
     );
   }
 }
 Counter.defaultProps = {
-  step:1
+  step: 1,
 };
 Counter.propTypes = {
-  step:PropTypes.number
+  step: PropTypes.number,
 };
 
 export default Counter;
+
+
