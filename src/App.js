f28520608efc8be5155b7db5e8438b2e0fcc23ad
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-07-05 12:02:23 +0300

    Add schemas

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import SignInForm from "./components/SignInForm";
-import { SCHEMA_SIGN_IN } from "./utils/schemaValidation";
-
+import { SCHEMA_SIGN_UP } from "./utils/schemaValidation";
 
 const App = () => {
   const userData = {
@@ -11,7 +10,15 @@ const App = () => {
     password:'elo&n71Qwe',
     age:'51',
   };
-  console.log(SCHEMA_SIGN_IN.validate(userData))
+  const userData2 = {
+    email:'elon@gmail.com',
+    password:'elo&n71Qwe',
+  };
+  console.log(SCHEMA_SIGN_UP.validate(userData)
+    .then((data)=>console.log(data))
+    .catch((err)=>console.log(err))
+  )  
+  // console.log(SCHEMA_SIGN_IN.validate(userData2)) //написать схему валидации входа
   return (
     <>
       <SignInForm />
