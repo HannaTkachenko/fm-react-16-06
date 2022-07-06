fe1ffae296b933ac0a74be300c9b3302055f73d7
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-07-06 10:28:04 +0300

    Add ErrorMessage

--- src/components/SignInForm/index.js
+++ src/components/SignInForm/index.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { Formik, Form, Field } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { SCHEMA_SIGN_IN } from "../../utils/schemaValidation";
+import styles from './SignInForm.module.scss';
 
 const initialValues = { email: "", password: "" };
 const SignInForm = (props) => {
@@ -11,13 +13,21 @@ const SignInForm = (props) => {
     formikBag.resetForm();
   };
   return (
-    <Formik initialValues={initialValues} onSubmit={onSubmit}>
+    <Formik
+      initialValues={initialValues}
+      validationSchema={SCHEMA_SIGN_IN}
+      onSubmit={onSubmit}
+    >
       {(formikProps) => {
         console.log("formikProps:", formikProps);
         return (
-          <Form>
-            <Field name="email"/>
-            <Field name="password"/>
+          <Form className={styles.form}>
+            <Field name="email" />
+            <ErrorMessage  name="email" component="em" />
+            {/* {formikProps.errors.email && formikProps.touched.email && <em>{formikProps.errors.email}</em>} */}
+            <Field name="password" />
+            <ErrorMessage  name="password" component="em" />
+            {/* {formikProps.errors.password && formikProps.touched.password && <em>{formikProps.errors.password}</em>} */}
             <input type="submit" value="send" />
           </Form>
         );
