35ceed0668971902ce4b4e8f7a6b20321718e85b
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-07-20 10:47:42 +0300

    Example separation state

--- src/components/LearnHooks/index.js
+++ src/components/LearnHooks/index.js
@@ -1,31 +1,31 @@
 import React, { useState } from "react";
 
 const LearnHooks = (props) => {
-  const [coords, setCoords] = useState({
-    x:0,
-    y:0,
-    count : 0
-  });
-  const handlerMouseMove = (event)=>{
+  const [coords, setCoords] = useState({ x: 0, y: 0 });
+  const [count, setCount] = useState(0);
+  const handlerMouseMove = (event) => {
     setCoords({
-      ...coords,
-      x:event.clientX,
-      y:event.clientY
-    })
-  }
+      x: event.clientX,
+      y: event.clientY,
+    });
+  };
   const handlerClick = () => {
-    setCoords({
-      ...coords,
-      count: coords.count+1
-    })
-  }
+    setCount((prevCount)=>prevCount+1);
+  };
   return (
-    <div style={{height:'60vh', backgroundColor:'pink', textAlign:'center', paddingTop:'10vh'}}
-    onMouseMove={handlerMouseMove}
-    onClick={handlerClick}>
+    <div
+      style={{
+        height: "60vh",
+        backgroundColor: "pink",
+        textAlign: "center",
+        paddingTop: "10vh",
+      }}
+      onMouseMove={handlerMouseMove}
+      onClick={handlerClick}
+    >
       <p>{coords.x}</p>
       <p>{coords.y}</p>
-      <p>{coords.count}</p>
+      <p>Count: {count}</p>
     </div>
   );
 };
