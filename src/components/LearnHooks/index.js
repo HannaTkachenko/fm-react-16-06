c43b7d38b7746e5b890e333f78a8dae1f2eee8bb
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-07-20 11:56:25 +0300

    BAD PRACTIC!!!

--- src/components/LearnHooks/index.js
+++ src/components/LearnHooks/index.js
@@ -2,20 +2,32 @@ import React, { useState, useEffect } from "react";
 
 const LearnHooks = (props) => {
   //console.log('render');
-  const [count, setCount] = useState(0);
-  const [step, setStep] = useState(10);
-  const handlerInput = ({target:{value}})=>{
-    setStep(Number(value))
-  }
+  // const [count, setCount] = useState(0);
+  // const [step, setStep] = useState(10);
+  const [state, setState] = useState({
+    count: 0,
+    step: 10,
+  });
+  const handlerInput = ({ target: { value } }) => {
+    setState({
+      ...state,
+      step: Number(value),
+    });
+  };
   useEffect(() => {
-    const handlerClick = () => {setCount(count=>count+step);};
+    const handlerClick = () => {
+      setState(({count})=>({
+        ...state,
+        count: count + state.step,
+      }));
+    };
     document.body.addEventListener("click", handlerClick);
-    console.log('addEventListener');
-    return ()=>{
+    console.log("addEventListener");
+    return () => {
       document.body.removeEventListener("click", handlerClick);
-      console.log('removeEventListener');
-    }
-  },[]);
+      console.log("removeEventListener");
+    };
+  }, [state.step]);
   return (
     <div
       style={{
@@ -26,8 +38,8 @@ const LearnHooks = (props) => {
       }}
       // onClick={handlerClick}
     >
-      <p>Count: {count}</p>
-      <input value={step} onChange={handlerInput}/>
+      <p>Count: {state.count}</p>
+      <input value={state.step} onChange={handlerInput} />
     </div>
   );
 };
