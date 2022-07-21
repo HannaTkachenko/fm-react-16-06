56cf687415ae139164b3c92e0459204be91b5ee4
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-07-21 10:43:34 +0300

    Add reset

--- src/components/FuncStopWatch/index.js
+++ src/components/FuncStopWatch/index.js
@@ -5,14 +5,16 @@ const FuncStopWatch = () => {
   const [time, setTime] = useState(new Date(0,0,0,0,0,0));
   const [isRunning, setIsRunning] = useState(false);
   const handlerBtn = ()=>{setIsRunning(!isRunning)}
+  const handlerBtnReset = ()=>{
+    setTime(new Date(0,0,0,0,0,0));
+    setIsRunning(false);
+  }
   useEffect(()=>{
-    console.log('useEffect', isRunning)
     if(isRunning){
       const idInterval = setInterval(()=>{
         setTime(time=>addMilliseconds(time, 1000))
       }, 1000);
       return ()=>{
-        console.log('clearInterval', isRunning)
         clearInterval(idInterval)
       }
     }
@@ -22,6 +24,7 @@ const FuncStopWatch = () => {
       <h2>Stop Watch</h2>
       <h3>{format(time, 'HH:mm:ss')}</h3>
       <button onClick={handlerBtn}>{isRunning?'Stop':'Start'}</button>
+      <button onClick={handlerBtnReset}>Reset</button>
     </div>
   );
 }
