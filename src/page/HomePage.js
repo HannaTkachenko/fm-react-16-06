0945907be14131d2f3c729689450dd0996aafeb8
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-07-22 10:59:39 +0300

    Add useMemo

--- src/page/HomePage.js
+++ src/page/HomePage.js
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React, {useState, useContext, useEffect, useCallback, useMemo} from 'react';
 import UserProfile from '../components/UserProfile';
 import { ThemeContext } from '../context';
 import CONSTANTS from "../constants";
@@ -7,12 +7,30 @@ const currentStyle = {
   [THEMES.LIGHT]:{backgroundColor:'#eee',color:'#222'},
   [THEMES.DARK]:{backgroundColor:'#222',color:'#eee'},
 }
-const HomePage = () => {
-  const [theme] = useContext(ThemeContext);
+function calcPow5Value(n){ //O(n)
+  let result = 0;
+  for(let i=1; i<1000000000; i++){result += i;}
+  return n**5;
+}
+const HomePage = (props) => {
+  const [value, setValue] = useState(10);
+  const [theme, setTheme] = useContext(ThemeContext);
+  const handlerTheme = useCallback( ()=>{
+    setTheme(theme===THEMES.LIGHT ? THEMES.DARK : THEMES.LIGHT);
+  },[theme])
+  const handlerInput = useCallback( ({target:{value}})=>{
+    setValue(Number(value))
+  }, [value])
+  const handlerLogValue = useCallback(()=>{console.log(value)},[value]);
+  const memocalcPow5Value = useMemo(()=>calcPow5Value(value), [value])
   return (
     <div style={currentStyle[theme]}>
       <h1>HomePage</h1>
+      <h2>value:{memocalcPow5Value}</h2>
+      <input type='number' value={value} onChange={handlerInput}/>
       <UserProfile />
+      <button onClick={handlerTheme}>switch theme</button>
+      <button onClick={handlerLogValue}>log value</button>
     </div>
   );
 }
